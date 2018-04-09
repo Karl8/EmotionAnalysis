0ced3a5390c1a280d1d7a1f8ee65fc1eea3bd3db
Captain_Mushroom <wuhuajun132@gmail.com>
2018-04-09 20:17:58 +0800

    Update download.py

--- download.py
+++ download.py
@@ -3,7 +3,30 @@
 import re
 import urllib,urllib2
 import os  
-      
+
+d = dict()
+
+d["amusement"] = 1
+d["anger"] = 2
+d["awe"] = 3
+d["contentment"] = 4
+d["disgust"] = 5
+d["excitement"] = 6
+d["fear"] = 7
+d["sadness"] = 8
+
+cnt = dict()
+
+cnt[1] = 1
+cnt[2] = 1
+cnt[3] = 1
+cnt[4] = 1
+cnt[5] = 1
+cnt[6] = 1
+cnt[7] = 1
+cnt[8] = 1
+
+
 def file_name(file_dir):   
     for root, dirs, files in os.walk(file_dir):  
         print(files) #当前路径下所有非目录子文件
@@ -14,17 +37,23 @@ def file_name(file_dir):
             print "file_name:", ff
             f = open("./dataset/" + ff, "r")
             try:
-                os.mkdir("./dataset/" + f_name[0])
+                os.mkdir("./dataset/" + "img")
             except:
                 print "dir exist"
-            cnt = 0
+
             for lines in f.readlines():
-                cnt += 1
                 line = lines.split(",");
+                if (int(line[2]) != 1 and int(line[2]) != 0):
+                    continue
                 imgurl = line[1]
-                download_name = "./dataset/" + f_name[0] + "/" + str(cnt) + "_" + line[2] + "_" + line[3].strip() + ".jpg"
+                download_name = "./dataset/" + "img" + "/" + str(d[line[0]]) + "_" + str(cnt[d[line[0]]]) + ".jpg"
                 print download_name
+                cnt[d[line[0]]] += 1
+                if os.path.exists(download_name):
+                    print "file_exist"
+                    continue
                 try:
+                    print "downloading file"
                     urllib.urlretrieve(imgurl, download_name)
                 except:
                     print "downloading img error"
